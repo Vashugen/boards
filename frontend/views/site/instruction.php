57bd04ca1ecb905269f6bedaacc0736bbb304195
VashuGen <vashu_gen@mail.ru>
2022-01-09 14:09:28 +0300

    how to work block

--- frontend/views/site/instruction.php
+++ frontend/views/site/instruction.php
@@ -11,8 +11,8 @@ $this->registerCssFile("/css/instruction.css");
                     <div class="record-container-column-wrapper">
                         <div class="record-tagline-text">
                             <div class="record-tagline-text-wrapper">
-                                <div class="record-tagline-text-main">New You Company (NYC)</div>
-                                <div class="record-tagline-text-text">Каждый путь начинается с первого шага!</div>
+                                <div class="record-tagline-text-main text-title">New You Company (NYC)</div>
+                                <div class="record-tagline-text-text text-description">Каждый путь начинается с первого шага!</div>
                             </div>
                         </div>
                     </div>
@@ -27,9 +27,34 @@ $this->registerCssFile("/css/instruction.css");
     <div class="record-block">
         <div class="record-div-introduce">
             <div class="record-container">
-                <div class="record-introduce-img"></div>
-                <div class="record-introduce-text"></div>
+                <div class="record-introduce-col">
+                    <div class="record-introduce-img"></div>
+                </div>
+                <div class="record-introduce-col">
+                    <div class="record-introduce-text-wrapper">
+                        <div class="record-introduce-text-align">
+                            <div class="record-introduce-text-title text-title">О нашей компании</div>
+                            <div class="record-introduce-text-key text-description">Ключевые слова о компании</div>
+                            <div class="record-introduce-text-line"></div>
+                            <div class="record-introduce-text-text text-description">Коротко представьтесь и расскажите о компании или сервисе в 3-4 строках. С какими клиентами вы работаете, что вас вдохновляет. Чем гордится ваша команда, какие у нее ценности и мотивация.</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
+<div class="record" style="line-height: normal; position: relative">
+    <div class="record-how">
+        <div class="record-div-how">
+            <div class="record-container">
+                <div class="record-how-col">
+                    <div class="record-how-text-title text-title">Как это работает<br></div>
+                    <div class="record-how-text-text text-description">Всё взаимодействие с плакатом укладывается в несколько простых, но максимально эффективных шагов</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
